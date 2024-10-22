7df8bf1e416833928a5618ca5c0926bba0262253
KenoLum <julio.garcial@uanl.edu.mx>
2024-10-21 19:33:50 -0600

    Update PC_PIA_PwGenerator.py

--- PIA E3/PC_PIA_PwGenerator.py
+++ PIA E3/PC_PIA_PwGenerator.py
@@ -1,12 +1,24 @@
-def password_generator():
-    import string
-    import random
+import string
+import random
+import logging
 
+def password_generator():
+
+    logging.basicConfig(
+        filename='PwGenerator.log',
+        level=logging.DEBUG,
+        format='| %(asctime)s | %(name)s | %(levelname)s | %(message)s'
+    )
+    
     while True:
         password_length = int(input("Introduce la longitud deseada de la contraseña (mínimo 8): "))
         use_special = input("¿Incluir caracteres especiales? (s/n): ").strip().lower() == 's'
+
+        logging.info(f"Longitud de la contraseña seleccionada: {password_length}")
+        logging.info(f"Incluir caracteres especiales: {'Sí' if use_special else 'No'}")
         
         if password_length < 8:
+            logging.warning("La longitud mínima de la contraseña es 8. El usuario ingresó una longitud no válida.")
             print("La longitud mínima de la contraseña debe ser 8.")
         else:
             break
@@ -19,5 +31,5 @@ def password_generator():
     for i in range(password_length):
         password += random.choice(characters)
 
+    logging.info("Contraseña generada exitosamente.")
     print(f"Contraseña generada: {password}")
-
